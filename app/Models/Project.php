9969d1284a6bb9146c748b32e9f02da5ce6ccec4
SevenErol <lorenzo.maggi1998@gmail.com>
2023-01-17 15:13:19 +0100

    pivot table and relationships established

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 use App\Models\Type;
+use App\Models\Technology;
 
 class Project extends Model
 {
@@ -18,5 +20,10 @@ class Project extends Model
     public function type(): BelongsTo
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
